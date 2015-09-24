5011414d51c8d0da11c2c6821e2a95969ee07922
harayoki <github@harayoki.net>
2015-09-24 22:44:59 +0900

    test

---
+++ src/typescript_template.js
@@ -0,0 +1,22 @@
+// Description
+//   typescript_template
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot hello - <what the respond trigger does>
+//   orly - <what the hear trigger does>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   foo <foo@example.com>
+
+function BotMain(robot) {
+  robot.respond(/hello/, function(res) {
+    msg.send('hello hello');
+  });
+}
+module.exports = BotMain;
